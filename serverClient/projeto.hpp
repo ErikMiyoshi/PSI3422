bcc2d5b5001f9375a2be24f1b3a5b78005c0786c
Erik <erikmiyoshi@hotmail.com>
2019-10-12 23:06:34 -0300

    "Testando classes server e client"

--- serverClient/projeto.hpp
+++ serverClient/projeto.hpp
@@ -46,11 +46,12 @@ public:
 };
 
 SERVER::SERVER() {
+  std::cout << "t1" << std::endl;
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE; // use my IP
-
+  std::cout << "t2" << std::endl;
   if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
     // return 1;
@@ -86,8 +87,9 @@ SERVER::SERVER() {
 SERVER::~SERVER() {}
 
 void SERVER::waitConnection() {
-  cout << "entrei conexao" << endl;
+  cout << "WaitConnection" << endl;
   while (1) {
+    cout << "esperando" << endl;
     sin_size = sizeof their_addr;
     new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
     if (new_fd == -1) {
@@ -103,13 +105,22 @@ void SERVER::waitConnection() {
 }
 
 void SERVER::sendBytes(int nBytesToSend, BYTE *buf) {
-  if (send(new_fd, buf, strlen((char *)buf) + 1, 0) == -1)
-    perror("send");
+  for (int i = 0; i < nBytesToSend; i++) {
+    if (send(new_fd, &buf[i], 1, 0) == -1)
+      perror("send");
+  }
+
+  /*if (send(new_fd, buf, strlen((char *)buf) + 1, 0) == -1)
+    perror("send");*/
 }
 
 void SERVER::receiveBytes(int nBytesToReceive, BYTE *buf) {
-  if (recv(new_fd, buf, MAXDATASIZE, 0) == -1)
-    perror("recv");
+  for (int i = 0; i < nBytesToReceive; i++) {
+    if (recv(new_fd, &buf[i], 1, 0) == -1)
+      perror("recv");
+  }
+  /*if (recv(new_fd, buf, MAXDATASIZE, 0) == -1)
+    perror("recv");*/
 }
 
 void *SERVER::get_in_addr(struct sockaddr *sa) {
@@ -144,8 +155,9 @@ CLIENT::CLIENT(string endereco) {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
 
-  char *s;
-  this->endereco.copy(s, this->endereco.length());
+  char s[this->endereco.size() + 1];
+  this->endereco.copy(s, this->endereco.length() + 1);
+  s[this->endereco.size()] = '\0';
   if ((rv = getaddrinfo(s, PORT, &hints, &servinfo)) != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
     // return 1;
@@ -176,13 +188,21 @@ CLIENT::CLIENT(string endereco) {
 CLIENT::~CLIENT() {}
 
 void CLIENT::sendBytes(int nBytesToSend, BYTE *buf) {
-  if (send(sockfd, buf, strlen((char *)buf) + 1, 0) == -1)
-    perror("send");
+  for (int i = 0; i < nBytesToSend; i++) {
+    if (send(sockfd, &buf[i], 1, 0) == -1)
+      perror("send");
+  }
+  /*if (send(sockfd, buf, strlen((char *)buf) + 1, 0) == -1)
+    perror("send");*/
 }
 
 void CLIENT::receiveBytes(int nBytesToReceive, BYTE *buf) {
-  if (recv(sockfd, buf, MAXDATASIZE, 0) == -1)
-    perror("recv");
+  for (int i = 0; i < nBytesToReceive; i++) {
+    if (recv(sockfd, &buf[i], 1, 0) == -1)
+      perror("recv");
+  }
+  /*if (recv(sockfd, buf, MAXDATASIZE, 0) == -1)
+    perror("recv");*/
 }
 
 void *CLIENT::get_in_addr(struct sockaddr *sa) {
--- serverClient/server6b.cpp
+++ serverClient/server6b.cpp
@@ -3,12 +3,12 @@
 #include "projeto.hpp"
 //#include <cekeikon.h>
 
-int main(void) {
+int main() {
   std::cout << "teste" << std::endl;
   SERVER server;
   std::cout << "teste2" << std::endl;
   server.waitConnection();
-
+  std::cout << "teste3" << std::endl;
   const int n = 100000;
   BYTE buf[n];
   memset(buf, 111, n);
